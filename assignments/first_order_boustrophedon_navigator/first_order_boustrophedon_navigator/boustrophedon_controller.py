d01ab7e8cc10053c70e743cb8dc574faaef3be9c
snitch-99 <kprashar@asu.edu>
2025-01-27 23:16:40 -0700

    Update boustrophedon_controller.py
    Tuned controller parameter for the best performance.
    Kp_linear    = 5.0
    Kp_angular = 9.5
    Kd_linear    = 0.1
    Kd_angular = 0.1
    Spacing      = 0.5
    max_speed = 2

--- assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
+++ assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
@@ -19,11 +19,11 @@ class BoustrophedonController(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('Kp_linear', 10.0),
+                ('Kp_linear', 5.0),
                 ('Kd_linear', 0.1),
-                ('Kp_angular', 5.0),
-                ('Kd_angular', 0.2),
-                ('spacing', 1.0)
+                ('Kp_angular', 9.5),
+                ('Kd_angular', 0.1),
+                ('spacing', 0.5)
             ]
         )
         
